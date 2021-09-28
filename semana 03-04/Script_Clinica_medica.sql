cdcba7686cf96030dbaf4a6fb3de23d1bec2ba17
Vitor <jszvitor@gmail.com>
2021-09-27 21:41:02 -0300

    feat(main): added insertions

--- semana 03-04/Script_Clinica_medica.sql
+++ semana 03-04/Script_Clinica_medica.sql
@@ -86,3 +86,32 @@ delimiter ;
 */
 
 
+-- populando o banco de dados
+-- inserções na relação Especialidades
+insert into Especialidade(CodEspec, NomeEspec, Descricao) values (0001, 'Clínico Geral', 'Consultas, exames, prescricoes.');
+insert into Especialidade(CodEspec, NomeEspec, Descricao) values (0002, 'Pediatria', 'Assistência à criança e ao adolescente.');
+insert into Especialidade values (0003, 'Odontologista', 'Cuidados bucais');
+-- select * from Especialidade;
+
+-- inserções na relação Clínica
+insert into Clinica(CodCli, NomeCli, Endereco, Telefone, Email) 
+values (1001, 'Renascer', 'Rua Exp. Guilherme Salvador, 57 - Recife', '81995374533', 'oi@renascer.com');
+insert into Clinica values (1002, 'Sim!', 'Av. Vieira Aguiar, 3234 - Olinda', '81995374537', 'contato@sim.com.br');
+insert into Clinica values (1003, 'Hapvida', 'Rod BR 101, km 30 - Recife', '81995373544', 'hapvida@sac.com.br');
+-- select * from Clinica;
+
+-- inserções na relação Paciente date: YYYY:MM:DD
+insert into Paciente (CpfPaciente, NomePac, DataNascimento, Genero, Telefone, Email)
+values ('12345678912', 'Valéria', '1980-01-22', 'F', '88995374501', 'val@gmail.com');
+insert into Paciente values ('12345678913', 'Jonh', '1982-01-23', 'M', '88995374573', 'amplificado@gmail.com');
+insert into Paciente values ('12345678914', 'Helena', '1999-01-23', 'F', '88995374574', 'Helleena@gmail.com');
+insert into Paciente values ('12345678915', 'Bonifácio', '2018-11-10', 'M', '88995374575', 'jose_bonifacio@ufrpe.br');
+
+-- inserções na associação Medico
+insert into Medico (CodMed, NomeMed, Genero, Telefone, Email, CodEspec)
+values (2001, 'Arlindo', 'M', '81995078534', 'arlindomorais@gmail.com', 0001);
+insert into Medico values (2002, 'Marília', 'F', '81995078534', 'marilia@ooutlook.com', 0002);
+insert into Medico values (2003, 'Vitória', 'F', '81995078534', 'vitoria@ooutlook.com', 0003);
+
+-- inserções na relação ClinicaMedico
+insert into ClinicaMedico () values ();
